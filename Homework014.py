941817096d0e606f1faa613d7f02479ea106d589
Edgar <sorata05@gmail.com>
2023-06-04 20:17:35 +0500

    Homework

--- Homework014.py
+++ Homework014.py
@@ -1,27 +1,10 @@
+def Chek(number):
+    for i in range(2, number):
+        if number % i == 0:
+            return (f"Число {number} не простое")
+
+        return (f"Число {number} простое")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+number = int(input("Ввeдите число: "))
+print(Chek(number))
